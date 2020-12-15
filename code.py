ff9f27474347f6742bae9aa8899bbc27978b8f5f
Izely <74884638+Izely@users.noreply.github.com>
2020-12-15 02:17:31 +0200

    checkin password

--- code.py
+++ code.py
@@ -7,3 +7,8 @@ from kivy import App
 #a storage folder
 #import relavent libraries
 #brush up on pyQt(greener is better ;))eryeeyyt
+for trials in range(0, 9):
+	password = input('password:')
+	if password = stored_pass:
+		pass
+	else: continue
