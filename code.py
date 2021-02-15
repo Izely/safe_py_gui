3de2caa97378fe49d5f7e0391ccc5f17541b036d
Izely <74884638+Izely@users.noreply.github.com>
2021-02-15 20:19:05 +0200

    Update code.py

--- code.py
+++ code.py
@@ -8,13 +8,14 @@ from kivy import App
 from kivy.md import App, Screen
 #variables///password/stored_pass/trials/encryption/wrong pass msg/exhausted chances msg/
 #password, username
-#an incryption method/hashing
+#an encryption method/hashing
 #a storage folder
 #import relavent libraries
 for trials in range(0, 9):
 	password = input('password:')
 	if password = stored_pass:
-		pass
+		pass                     #decrypt files
+		break
 	else: continue
 
 
